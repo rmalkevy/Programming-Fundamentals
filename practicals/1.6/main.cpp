587058a34f7b8f31c00716b631d905ef7fa9e87e
Malkevych Roman <henprokuror@gmail.com>
2024-09-20 10:45:30 +0300

    refacted

--- practicals/1.6/main.cpp
+++ practicals/1.6/main.cpp
@@ -107,7 +107,7 @@ int main() {
     int n = 10;
 
     std::vector<int> a(n); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    std::vector<int> b(n);
+    std::vector<int> b(n); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     for (int i = 0; i < n; i++) {
       a[i] = rand() % 2;
