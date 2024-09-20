2326c8dcb017fa5e7f8215fe492de27c541b5a84
Malkevych Roman <henprokuror@gmail.com>
2024-09-20 12:45:26 +0300

    minor

--- practicals/1.6/main.cpp
+++ practicals/1.6/main.cpp
@@ -21,25 +21,25 @@ bool areEqual(int a, int b) {
   return true;
 }
 
-// bool isMultipleOf8(int a) {
-//   // 7 in binary is 00000111, so if a & 7 == 0, then a is a multiple of 8
-//   // because 8 in binary is 00001000
-//   // and 00001000 & 00000111 == 00000000
-//   // so if a & 7 == 0, then a is a multiple of 8
-//   // 00011000 -> 24
-//   // 00111000 -> 56
-//   // 01111000 -> 120
-//   // 11111000 -> 248
-//   int mask_binary = 0b00000111; // 7 in binary
-//   return (a & mask_binary) == 0;
-// }
+bool isMultipleOf8(int a) {
+  // 7 in binary is 00000111, so if a & 7 == 0, then a is a multiple of 8
+  // because 8 in binary is 00001000
+  // and 00001000 & 00000111 == 00000000
+  // so if a & 7 == 0, then a is a multiple of 8
+  // 00011000 -> 24
+  // 00111000 -> 56
+  // 01111000 -> 120
+  // 11111000 -> 248
+  int mask_binary = 0b00000111; // 7 in binary
+  return (a & mask_binary) == 0;
+}
 
-// void printVector(const std::vector<int>& vec) {
-//   for (int i = 0; i < vec.size(); i++) {
-//     std::cout << vec[i] << " ";
-//   }
-//   std::cout << std::endl;
-// }
+void printVector(const std::vector<int>& vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    std::cout << vec[i] << " ";
+  }
+  std::cout << std::endl;
+}
 
 int main() {
   {
@@ -57,81 +57,76 @@ int main() {
     }
   }
 
-  // {
-  //   // Задано ціле значення А. Визначити, чи є значення А
-  //   // кратним 8.
+  {
+    // Задано ціле значення А. Визначити, чи є значення А
+    // кратним 8.
 
-  //   int a;
-  //   std::cout << "Enter an integer: ";
-  //   std::cin >> a;
+    int a;
+    std::cout << "Enter an integer: ";
+    std::cin >> a;
 
-  //   if (isMultipleOf8(a)) {
-  //     std::cout << "The number is a multiple of 8." << std::endl;
-  //   } else {
-  //     std::cout << "The number is not a multiple of 8." << std::endl;
-  //   }
-  // }
+    if (isMultipleOf8(a)) {
+      std::cout << "The number is a multiple of 8." << std::endl;
+    } else {
+      std::cout << "The number is not a multiple of 8." << std::endl;
+    }
+  }
 
-  // {
-  //   // Завдання:
-  //   // Задано дві послідовності, які складаються з 0 та 1.
-  //   // Скласти специфікацію для моделювання операцій AND, OR, XOR.
+  {
+    // Завдання:
+    // Задано дві послідовності, які складаються з 0 та 1.
+    // Скласти специфікацію для моделювання операцій AND, OR, XOR.
 
-  //   // Специфікація:
-  
-  //   // Вхідні дані:
-  //   // - Дві послідовності бітів (A, B) однакової довжини.
+    // Специфікація:
 
-  //   // Алгоритм:
-  //   // 1. Пройти по кожному елементу послідовностей A і B.
-  //   // 2. Для кожного елемента обчислити результат операції AND, OR, або XOR.
-  //   // 3. Зберегти результат в нову послідовність.
+    // Вхідні дані:
+    // - Дві послідовності бітів (A, B) однакової довжини.
 
-  //   // Операції:
-  //   // - AND: ri = ai AND bi
-  //   // - OR: ri = ai OR bi
-  //   // - XOR: ri = ai XOR bi
+    // Алгоритм:
+    // 1. Пройти по кожному елементу послідовностей A і B.
+    // 2. Для кожного елемента обчислити результат операції AND, OR, або XOR.
+    // 3. Зберегти результат в нову послідовність.
 
-  //   // Вихідні дані:
-  //   // - Результуюча послідовність R.
+    // Операції:
+    // - AND: ri = ai AND bi
+    // - OR: ri = ai OR bi
+    // - XOR: ri = ai XOR bi
 
-  //   int n = 10;
+    // Вихідні дані:
+    // - Результуюча послідовність R.
 
-<<<<<<< Updated upstream
+    int n = 10;
+
     std::vector<int> a(n); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    std::vector<int> b(n); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-=======
-  //   std::vector<int> a(n); // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  //   std::vector<int> b(n);
->>>>>>> Stashed changes
+    std::vector<int> b(n);
 
-  //   for (int i = 0; i < n; i++) {
-  //     a[i] = rand() % 2;
-  //     b[i] = rand() % 2;
-  //   }
+    for (int i = 0; i < n; i++) {
+      a[i] = rand() % 2;
+      b[i] = rand() % 2;
+    }
 
-  //   std::cout << "A  : ";
-  //   printVector(a);
-  //   std::cout << "B  : ";
-  //   printVector(b);
+    std::cout << "A  : ";
+    printVector(a);
+    std::cout << "B  : ";
+    printVector(b);
 
-  //   std::vector<int> r_and(n);
-  //   std::vector<int> r_or(n);
-  //   std::vector<int> r_xor(n);
+    std::vector<int> r_and(n);
+    std::vector<int> r_or(n);
+    std::vector<int> r_xor(n);
 
-  //   for (int i = 0; i < n; i++) {
-  //     r_and[i] = a[i] & b[i];
-  //     r_or[i] = a[i] | b[i];
-  //     r_xor[i] = a[i] ^ b[i];
-  //   }
+    for (int i = 0; i < n; i++) {
+      r_and[i] = a[i] & b[i];
+      r_or[i] = a[i] | b[i];
+      r_xor[i] = a[i] ^ b[i];
+    }
 
-  //   std::cout << "AND: ";
-  //   printVector(r_and);
-  //   std::cout << "OR : ";
-  //   printVector(r_or);
-  //   std::cout << "XOR: ";
-  //   printVector(r_xor);
-  // }
+    std::cout << "AND: ";
+    printVector(r_and);
+    std::cout << "OR : ";
+    printVector(r_or);
+    std::cout << "XOR: ";
+    printVector(r_xor);
+  }
 
   return 0;
 }
