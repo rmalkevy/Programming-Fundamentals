6f3bd31458d1fd1a2c3cf1d6ccbfa64f37c4b1ed
Malkevych Roman <henprokuror@gmail.com>
2025-10-03 11:02:37 +0300

    added electric board dummy example

--- practicals/1.4/main.cpp
+++ practicals/1.4/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-int main() {
+int add(int a, int b) {
+    return a + b;
+}
+
+int arithmeticMean(int a, int b) {
     double b1, q;
     int n;
-    
+
     // Введення даних
     std::cout << "Введіть перший член прогресії (b1): ";
     std::cin >> b1;
     std::cout << "Введіть знаменник прогресії (q): ";
     std::cin >> q;
     std::cout << "Введіть кількість елементів (n): ";
-    std::cin >> n;
+    // std::cin >> n;
 
+    n = 11;
     double sum = 0;  // Змінна для зберігання суми елементів
     double current_element = b1;  // Поточний член прогресії
     
@@ -20,12 +25,20 @@ int main() {
         sum += current_element;  // Додаємо поточний елемент до суми
         current_element *= q;    // Переходимо до наступного елемента (множимо на q)
     }
-    
+
     // Обчислюємо середнє арифметичне
     double S_avg = sum / n;
 
     // Виведення результату
     std::cout << "Середнє арифметичне перших " << n << " елементів: " << S_avg << std::endl;
+}
+
+int main() {
+    int result = add(1, 2);
+    std::cout << "Result: " << result << std::endl;
+
+    int arithmeticMeanResult = arithmeticMean(1, 2);
+    std::cout << "Arithmetic mean result: " << arithmeticMeanResult << std::endl;
 
     return 0;
 }
